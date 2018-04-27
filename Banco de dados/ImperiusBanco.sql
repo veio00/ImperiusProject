76a754cecc4a7872d347390decea05d5687e85f9
Batman <50036@alunos.bandtec.com.br>
2018-04-26 23:03:11 -0300

    Script Banco de dados

---
+++ Banco de dados/ImperiusBanco.sql
@@ -0,0 +1,98 @@
+create table Acesso(
+idAcesso int primary key identity(1,1),
+Descricao_Acesso varchar(100),
+Nome_Acesso varchar(15)
+)
+
+create table Grupo(
+idGrupo int primary key identity(1,1),
+Nome_grupo varchar(150)
+)
+
+create table Cliente(
+idCliente int primary key identity(1,1),
+Nome varchar(100),
+Email varchar(100),
+Acesso_Cliente int foreign key references Acesso(idAcesso),
+Grupo_Cliente int foreign key references Grupo(idGrupo),
+)
+
+Create table Maquina(
+idMaquina int primary key identity(1,1),
+Responsavel varchar(50),
+Nome_Maquina varchar(30),
+Adiquirida varchar(10),
+Sistema varchar(20),
+Keep_Alive int,
+Grupo_Cliente int foreign key references Grupo(idGrupo)
+)
+
+create table Processador(
+idCpu int primary key identity(1,1),
+Modelo varchar(100),
+Maquina_Cpu int foreign key references Maquina(idMaquina)
+)
+
+create table Memoria(
+idMemoria int primary key identity(1,1),
+Qtd int,
+Geracao varchar(4),
+Maquina_Memoria int foreign key references Maquina(idMaquina)
+)
+
+create table Disco(
+idDisco int primary key identity(1,1),
+Espaco int ,
+Marca varchar(100),
+N_Discos int,
+Maquina_Disco int foreign key references Maquina(idMaquina)
+)
+
+create table Leitura(
+idLeitura int primary key identity(1,1),
+Hd int,
+Mram int,
+Cpu int,
+Maquina_Uso int foreign key references Maquina(idMaquina)
+)
+
+
+create table Logss(
+idLogs int primary key identity(1,1),
+Data date,
+Leitura_Logs int foreign key references Leitura(idLeitura)
+)
+
+
+/*select * from Grupo*/
+select * from Maquina where Grupo_Cliente = 1
+select * from leitura where Maquina_Uso= 1
+select * from Maquina inner join Processador on Maquina_Cpu = idMaquina inner join Memoria on Maquina_Memoria = idMaquina inner join Disco on Maquina_Disco = idMaquina
+
+
+insert into Acesso(Descricao_Acesso,Nome_Acesso) values('So os picas usam', 'Administrador')
+insert into Acesso(Descricao_Acesso,Nome_Acesso) values('O resto', 'Cliente')
+
+insert into Grupo values('Imperius')
+insert into Grupo values('Logicalis')
+
+insert into Cliente(Nome,Email,Acesso_Cliente,Grupo_Cliente) values('Carlos Vinicius','bebe@nubeliu.com.br',2,1)
+insert into Cliente(Nome,Email,Acesso_Cliente,Grupo_Cliente) values('Wilian Mathias','veio@nubeliu.com.br',1,1)
+insert into Cliente(Nome,Email,Acesso_Cliente,Grupo_Cliente) values('Adrelayne ','dodoi@nubeliu.com.br',2,1)
+
+insert into Maquina values('Will','Teste-pc0',getdate(),'Microsoft',1,1)
+insert into Maquina values('Will','Teste-pc1',getdate(),'Microsoft',0,1)
+insert into Maquina values('Will','Teste-pc2',getdate(),'Microsoft',1,1)
+insert into Maquina values('Will','Teste-pc3',getdate(),'Microsoft',0,1)
+insert into Maquina values('Will','Teste-pc4',getdate(),'Microsoft',1,1)
+insert into Maquina values('Will','Teste-pc5',getdate(),'Microsoft',0,1)
+
+insert into Leitura(Hd,Mram,Cpu,Maquina_Uso) values(44,80,66,1)
+
+insert into Processador('core i3',1)
+
+insert into Memoria(Qtd,Geracao,Maquina_Memoria) values (2048,'ddr3',1)
+
+insert into Disco(Espaço,Marca,N_Discos,Maquina_Disco) values (248000,'seagate', 1 , 1)
+
+select max(idMAquina) from Maquina
