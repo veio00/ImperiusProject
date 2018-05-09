aeb33e9bb442fcf67680d51beb8e2d67adf170e3
Batman <50036@alunos.bandtec.com.br>
2018-05-09 18:33:05 -0300

    codigo da @adrelayne js funcionando

--- Paginas WEB/js/relatorio.js
+++ Paginas WEB/js/relatorio.js
@@ -1,9 +1,16 @@
 $(document).ready (function () {
-	geraRelatorio();
+	geraRelatorio2();
 	function geraRelatorio (){
             $.ajax({
-                url: "https://jsonplaceholder.typicode.com/users/",
-                type: "GET",
+                async: false,
+				url: "http://imperius.azurewebsites.net/api/view/CarregaMaquina",
+                data: {grupo:1},
+				method: "POST",
+				dataType: "json",
+				headers: {
+				"Cache-Control": "no-cache",
+				"Postman-Token": "df98e587-f312-4bab-b892-cab75b40d4be"
+				},
                 success: function (data) {
                     $('#example').DataTable({
                         stateSave: true,
@@ -29,11 +36,15 @@ $(document).ready (function () {
                         paging: true,
                         data: data,
                         columns: [ //configuração de pesquisa das tabelas
-                            { "data": "id" },
-							{ "data": "name" },
-							{ "data": "username" },
-							{ "data": "email" },
-							{ "data": "address" }
+                            { "data": "idMaquina" },
+							{ "data": "Responsavel" },
+							{ "data": "Nome_Maquina" },
+							{ "data": "Adiquirida" },
+							{ "data": "Sistema" },
+							{ "data": "Keep_Alive" },
+							{ "data": "Grupo_Cliente" },
+							{ "data": "idGrupo" },
+							{ "data": "Nome_Grupo" }
                         ]
                     });
                 },
@@ -41,5 +52,57 @@ $(document).ready (function () {
                     alert("Erro de Carregamento de Dados");
                 }
             });
+	}
+	
+	function geraRelatorio2 (){
+		var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "http://imperius.azurewebsites.net/api/view/CarregaMaquina?grupo=1",
+  "method": "POST",
+  "headers": {
+    "Cache-Control": "no-cache",
+    "Postman-Token": "5a170e39-2218-a3f9-dcbb-f6cd553aa831"
+  }
+}
+
+$.ajax(settings).done(function (data) {
+  console.log(data);
+  $('#example').DataTable({
+                        stateSave: true,
+                        "language": {
+                            "lengthMenu": "Mostrar MENU",
+                            "zeroRecords": "Nada encontrado",
+                            "info": "Total de PAGE de PAGES",
+                            "infoEmpty": "Nenhum Dado Encontrado",
+                            "search": "Filtrar:",
+                            "infoFiltered": "(filtered from MAX total records)",
+                            "paginate": {
+                                "first": "Primeira",
+                                "last": "Ultima",
+                                "next": "Proxima",
+                                "previous": "Anterior"
+                            }
+                        },
+                        "dom": '<"top"f l>t<"bottom"ip >',//configuração de menu
+                        "pagingType": "full_numbers",
+                        "scrollX": true,
+                        "scrollY": false,
+                        scrollCollapse: true,
+                        paging: true,
+                        data: data,
+                        columns: [ //configuração de pesquisa das tabelas
+                            { "data": "idMaquina" },
+							{ "data": "Responsavel" },
+							{ "data": "Nome_Maquina" },
+							{ "data": "Adiquirida" },
+							{ "data": "Sistema" },
+							{ "data": "Keep_Alive" },
+							{ "data": "Grupo_Cliente" },
+							{ "data": "idGrupo" },
+							{ "data": "Nome_Grupo" }
+                        ]
+                    });
+});
 	}
 });
