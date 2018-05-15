e1450a55a48b717c6ebdea302fcedfa2b36d8c1a
vinipis <cvf.999@gmail.com>
2018-05-15 20:58:00 -0300

    Criação de metodos par validação da mensageria

--- Coletor/src/br/com/imperius/coletor/controller/ValidadorAlerta.java
+++ Coletor/src/br/com/imperius/coletor/controller/ValidadorAlerta.java
@@ -7,8 +7,19 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Properties;
+import model.Leitura;
 
 public class ValidadorAlerta {
+
+    private Leitura leitura;
+
+    public Leitura getLeitura() {
+        return leitura;
+    }
+
+    public void setLeitura(Leitura leitura) {
+        this.leitura = leitura;
+    }
 
     public static void main(String args[]) throws IOException {
         GregorianCalendar gcalendar = new GregorianCalendar();
@@ -21,7 +32,59 @@ public class ValidadorAlerta {
         Properties props = getProp();
         String ultimaHora = props.getProperty("ultimaHora");
     }
+
+    public static void validacao(Leitura uso) {
+        int cpu = uso.getCpu();
+        int hd = uso.getHd();
+        int mram = uso.getMram();
+    }
+
+    public static void cpuPorcentagem(int cpu) {
+
+        if (cpu > 74 && cpu <= 84) {
+            //P3
+        } else if (cpu >= 85 && cpu <= 94) {
+            //P2
+        } else if (cpu >= 95) {
+            //P1
+        } else {
+            //qualquer coisa menor que 74 faz nada
+        }
+    }
+
+    public static void hdPorcentagem(int hd) {
+        if (hd > 79 && hd <= 80) {
+            //P3
+        } else if (hd >= 81 && hd <= 89) {
+            //P2
+        } else if (hd >= 90) {
+            //P1
+        } else {
+            //qualquer coisa menor que 79 faz nada
+        }
+    }
+
+    public static void mramPorcentagem(int mram) {
+        if (mram > 69 && mram <= 79) {
+            //P3
+        } else if (mram >= 80 && mram <= 89) {
+            //P2
+        } else if (mram >= 90) {
+            //P1
+        } else {
+            //qualquer coisa menor que 69 faz nada
+        }
+    }
+
     public static void p3() {
-        
+
+    }
+
+    public static void p2() {
+
+    }
+
+    public static void p1() {
+
     }
 }
